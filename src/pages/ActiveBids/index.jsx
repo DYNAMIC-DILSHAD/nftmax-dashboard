26d1da1e6187f6b3f84693c9a5f304317831f3ba
dilshad ansari <dilshadmanvi@gmail.com>
2024-03-12 16:57:39 +0530

    augdone

--- src/pages/ActiveBids/index.jsx
+++ src/pages/ActiveBids/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import datas from "../../mock_data/product_data.json";
-import UpdateTable from "./UpdateTable";
 import AllBidsSection from "./AllBidsSection";
 import CounterSection from "./CounterSection";
 import OverviewSection from "./OverviewSection";
@@ -12,7 +11,6 @@ export default function ActiveBids() {
         <CounterSection className="mb-10" />
         <OverviewSection className="mb-10" />
         <AllBidsSection allBids={allBids} className="mb-10" />
-        <UpdateTable className="mb-10" />
     </>
   );
 }
