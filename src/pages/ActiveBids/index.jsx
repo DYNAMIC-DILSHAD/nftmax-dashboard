f2bbc2e678466fe1916deed9a58ccc1f9de2d284
dilshad ansari <dilshadmanvi@gmail.com>
2024-03-12 12:22:26 +0530

    augdone

--- src/pages/ActiveBids/index.jsx
+++ src/pages/ActiveBids/index.jsx
@@ -9,8 +9,8 @@ export default function ActiveBids() {
   const allBids = datas.datas;
   return (
     <>
-        <CounterSection className="mb-10" />
         <OverviewSection className="mb-10" />
+        <CounterSection className="mb-10" />
         <AllBidsSection allBids={allBids} className="mb-10" />
         <UpdateTable className="mb-10" />
     </>
